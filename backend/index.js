b62f0e69602ff25affa7f6d372d92de408a847de
lata bansavad <latabansavade24@gmail.com>
2024-09-14 17:02:30 +0530

    3rd

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/news', newsRoutes);
   
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://frontend-olive-sigma.vercel.app/',
     credentials: true
 }));
 
